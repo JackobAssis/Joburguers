acbe3986e3c917efcad956e14122c951b2cd436e
JackobAssis <whybson@hotmail.com.br>
2025-11-15 00:53:06 -0300

    erro com admin

--- js/admin.js
+++ js/admin.js
@@ -519,15 +519,6 @@ function getLevelLabel(level) {
     return labels[level] || 'Bronze';
 }
 
-function formatPhone(phone) {
-    const cleaned = phone.replace(/\D/g, '');
-    const match = cleaned.match(/^(\d{2})(\d{5})(\d{4})$/);
-    if (match) {
-        return `(${match[1]}) ${match[2]}-${match[3]}`;
-    }
-    return phone;
-}
-
 // ========================================
 // PROMOÇÕES
 // ========================================
